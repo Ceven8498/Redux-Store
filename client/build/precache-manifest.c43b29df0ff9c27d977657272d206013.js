8a49df5a9f846eb5ad5176562539002ef07fbb18
CEven98 <doomfire98@gmail.com>
2020-11-08 05:52:23 -0600

    building app, added more redux

---
+++ client/build/precache-manifest.c43b29df0ff9c27d977657272d206013.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d737a2ccf57376fb9885694ed624b47c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea88285509c97f812fe1",
+    "url": "/static/css/main.6816d842.chunk.css"
+  },
+  {
+    "revision": "cc8b4fd4a4b38db92600",
+    "url": "/static/js/2.c7cd522c.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.c7cd522c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea88285509c97f812fe1",
+    "url": "/static/js/main.7497146d.chunk.js"
+  },
+  {
+    "revision": "347e1f4222debd5b2581",
+    "url": "/static/js/runtime-main.f59b1054.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
